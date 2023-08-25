932f6184bfbcd0aa65aa8f6d608094fe92a2bf93
James Faina <james@faina.co.uk>
2023-08-25 12:05:44 +0100

    Add tmux.conf

--- setup.sh
+++ setup.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 ## sets up the environment
 
-if [[ -f "bashrc" ]]; then
-  echo "Moving bashrc"
-  cp $HOME/.bashrc old_bashrc
-  cp bashrc $HOME/.bashrc
+if [[ -f "$HOME/.bashrc" ]]; then
+  echo "# " $(date) >> $HOME/old_bashrc
+  cat $HOME/.bashrc >> $HOME/old_bashrc
 fi
+cp bashrc $HOME/.bashrc
 
-if [[ -f "vimrc" ]]; then
-  echo "Moving vimrc"
-  cp $HOME/.vimrc old_vimrc
-  cp vimrc $HOME/.vimrc
+if [[ -f "$HOME/.vimrc" ]]; then
+  echo "# " $(date) >> $HOME/old_vimrc
+  cat $HOME/.vimrc >> $HOME/old_vimrc
 fi
+cp vimrc $HOME/.vimrc
+
+if [[ -f "$HOME/.tmux.conf" ]]; then
+  echo "# " $(date) >> $HOME/old_tmux.conf
+  cat $HOME/.tmux.conf >> $HOME/old_tmux.conf
+fi
+cp tmux.conf $HOME/.tmux.conf
